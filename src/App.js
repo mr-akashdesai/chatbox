ea78e48938e958ea33a70be3138c0bc412466c51
Akash Desai <mr.akashdesai@gmail.com>
2020-09-24 13:30:10 +0100

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+/import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hoooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
